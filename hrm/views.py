1116d0acdf99a6a99e9faa67bbdfea867ec5340e
nivlapeter <nivlapeter.0549@gmail.com>
2020-11-03 15:35:32 +0300

    Add project structure

---
+++ hrm/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+#from .models import
+#from .forms import
+from rest_framework.response import Response
+from rest_framework.views import APIView
+#from .serializer import
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
