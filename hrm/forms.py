aef1c2f9a969800d4eb559adc9c45ae5031049d2
kelvin-daniel <kaymutor@gmail.com>
2020-11-04 22:22:18 +0300

    register models to admin

--- hrm/forms.py
+++ hrm/forms.py
@@ -2,15 +2,3 @@ from django import forms
 from .models import *
 #from .models import
 
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model=Profile
-
-class CompanyForm(forms.ModelForm):
-    class Meta:
-        model=Company
-
-class Make_userForm(forms.ModelForm):
-    class Meta:
-        model=Make_user
