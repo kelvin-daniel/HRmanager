2a2e208d967f9d9fcd4b97a88180e6997c125d30
kelvin-daniel <kaymutor@gmail.com>
2020-11-04 08:33:05 +0300

    add models

--- hrm/forms.py
+++ hrm/forms.py
@@ -1,2 +1,11 @@
 from django import forms
 #from .models import
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
