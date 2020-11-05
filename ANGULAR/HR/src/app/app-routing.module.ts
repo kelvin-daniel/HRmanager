5484d03521be7ef550657b4c790b9d482ba8be65
CindyKatoni <katmuema@gmail.com>
2020-11-05 18:17:08 +0300

    hr department routing

--- ANGULAR/HR/src/app/app-routing.module.ts
+++ ANGULAR/HR/src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { HrmanageradminComponent } from './hrmanageradmin/hrmanageradmin.component';
+import { CompanyadminComponent } from './companyadmin/companyadmin.component';
+import { TeammanagerComponent } from './teammanager/teammanager.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'signup',component:SignupComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'hrmanageradmin',component:HrmanageradminComponent},
+  {path:'companyadmin',component:CompanyadminComponent},
+  {path:'teammanager',component:TeammanagerComponent},
+  {path:'user',component:UserComponent},
 ];
 
 @NgModule({
