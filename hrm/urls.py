91f2af2d25f82701625204431b264f5b9fd06269
kelvin-daniel <kaymutor@gmail.com>
2020-11-05 12:55:46 +0300

    add api urls

--- hrm/urls.py
+++ hrm/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     #url('', views.index, name = 'index'),
-    path('api/company/', views.companyList.as_view())
-    path('api/company/', views.companyList.as_view())
+    path('api/company/', views.CompanyList.as_view()),
+    path('api/profile/', views.ProfileList.as_view()),
+    path('api/employee/', views.EmployeeList.as_view()),
 
 ]
