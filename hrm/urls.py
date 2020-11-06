e9a3ae29abfc14eee16a6a088ae7eaca536d3c8d
kelvin-daniel <kaymutor@gmail.com>
2020-11-06 10:26:33 +0300

    solve views error

--- hrm/urls.py
+++ hrm/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     # path('', views.index, name = 'index'),
-    path('leave/apply/',views.leave_creation,name='createleave'),
+    path('leave_creation/',views.Leave_creation,name='createleave'),
     path('leaves/pending/all/',views.leaves_list,name='leaveslist'),
     path('leaves/all/view/<int:id>/',views.leaves_view,name='userleaveview'),
     path('api/company/', views.CompanyList.as_view()),
@@ -14,7 +14,6 @@ urlpatterns=[
     path('supervisor/<int:pk>/', views.TeamManagerProfileView.as_view()),
     path('members/', views.MembersView.as_view()),
     path('team/<int:pk>/', views.TeamView.as_view()),
-
 ]
 
 if settings.DEBUG:
